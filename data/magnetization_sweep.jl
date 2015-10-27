c1afe0ff62421defe5f344ac47eb424856b89b78
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2015-10-26 22:23:55 -0200

    Corrected PhaseDiagram plot

--- data/magnetization_sweep.jl
+++ data/magnetization_sweep.jl
@@ -9,14 +9,16 @@ for ρi in 0:0.001:0.999 for βi in 0.01:0.01:10
     soc  = Society(n=N, ρ=ρi, ϵ=ϵ)
     iter, x = metropolis!(soc, β=βi)
 
-    mag[findin(0:0.001:0.999, ρi), findin(0.01:0.01:10, βi)] = abs(magnetization(soc, x))
+    mag[findin(0.01:0.01:10, βi), findin(0:0.001:0.999, ρi)] = abs(magnetization(soc, x))
 
     println("Evaluated point ρ=$ρi, β=$βi")
 end end
 
 save("data/magnetization.jld", "mag", mag)
 
-imshow(mag, extent=[0, 1, 0, 10], aspect="auto", interpolation="nearest")
+imshow(mag, extent=[0, 1, 0, 10], aspect="auto", interpolation="nearest", origin="lower")
+xlabel(L"\rho")
+ylabel(L"\beta")
 colorbar()
 
-savefig("data/magnetization.png")
+savefig("data/magnetization_eps0.1.png")
