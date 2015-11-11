08ca233c8182644ddc40520b0944200944c7a2ef
Lucas Silva Simões <simoes.lucas.silva@gmail.com>
2015-11-10 22:53:32 -0200

    More tests

--- data/magnetization_sweep.jl
+++ data/magnetization_sweep.jl
@@ -1,26 +1,50 @@
 using SocialSystems, JLD, PyPlot
 
-N   = 100
-mag = zeros(1000, 100, 10)
+N     = 100
+βsize = 8
+βrng  = linspace(10/βsize, 10, βsize)
+ρsize = 500
+ρrng  = linspace(1/ρsize, 1, ρsize)
+ϵsize = 100
+ϵrng  = linspace(0.5/ϵsize, 0.5, ϵsize)
 
-for βi in 0.01:0.01:10 for ρi in 0.01:0.01:1 for ϵi in 0.05:0.05:0.5
+mag   = zeros(βsize, ρsize, ϵsize)
+
+for βi in βrng for ρi in ρrng for ϵi in ϵrng
     soc  = Society(n=N, ρ=ρi, ϵ=ϵi)
     iter, x = metropolis!(soc, β=βi)
 
-    mag[findin(0.01:0.01:10, βi), findin(0.01:0.01:1, ρi), findin(0.05:0.05:0.5, ϵi)] = abs(magnetization(soc, x));
+    mag[findin(βrng, βi), findin(ρrng, ρi), findin(ϵrng, ϵi)] = abs(magnetization(soc, x));
 
     println("Evaluated point β=$βi, ρ=$ρi, ϵ=$ϵi")
 end end end
 
-save("data/magnetization_1000_100_10.jld", "mag", mag)
+save( "data/magnetization_$(βsize)_$(ρsize)_$(ϵsize).jld", "mag", mag)
 
-imshow(reshape(mag[:, :, findin(0.05:0.05:0.5, 0.1)], 1000, 100), extent=[0, 1, 0, 10], aspect="auto", interpolation="bicubic", origin="lower")
+imshow(reshape(mag[:, :, findin(ϵrng, 0.1)], βsize, ρsize), extent=[0, 1, 0, 10], aspect="auto", origin="lower", interpolation="bicubic")
 xlabel(L"\rho")
 ylabel(L"\beta")
 colorbar()
+# title(L"Magnetization, $\epsilon =  0.1$, $βsize \times ρsize$ points")
 
-savefig("data/magnetization_1000_100_10_rhobeta.png")
+savefig( "data/magnetization_$(βsize)_$(ρsize)_$(ϵsize)_rhobeta.png")
 
-# real	581m19.518s
-# user	581m1.290s
-# sys	0m28.770s
+clf()
+
+imshow(reshape(mag[:, findin(ρrng, 0.3), :], βsize, ϵsize), extent=[0, 0.5, 0, 10], aspect="auto", origin="lower", interpolation="bicubic")
+xlabel(L"\epsilon")
+ylabel(L"\beta")
+colorbar()
+# title(L"Magnetization, $\rho =  0.3$, $(βsize) \times $(ϵsize)$ points")
+
+savefig( "data/magnetization_$(βsize)_$(ρsize)_$(ϵsize)_epsbeta.png")
+
+clf()
+
+imshow(reshape(mag[findin(βrng, 8.75), :, :], ρsize, ϵsize), extent=[0, 1, 0, 0.5], aspect="auto", origin="lower", interpolation="gaussian")
+xlabel(L"\rho")
+ylabel(L"\epsilon")
+colorbar()
+# title(L"Magnetization, $\beta =  5.0$, $(ρsize) \times $(ϵsize)$ points")
+
+savefig( "data/magnetization_$(βsize)_$(ρsize)_$(ϵsize)_rhoeps.png")
