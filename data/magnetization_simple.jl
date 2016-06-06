66a101a84477dbe146af9806da029483e035f82e
Lucas Simões <simoes.lucas.silva@gmail.com>
2016-06-06 16:41:01 -0300

    More stuff; need to analyze

--- data/magnetization_simple.jl
+++ data/magnetization_simple.jl
@@ -3,33 +3,34 @@ using SocialSystems, JLD, PyPlot
 ## Setting the variables
 ###########################
 N     = 100
-βsize = 200
-βrng  = linspace(10/βsize, 10, βsize)
-ρsize = 200
-ρrng  = linspace(1/ρsize, 1, ρsize)
-ϵ 	  = 0.
+βsize = 100
+βrng  = linspace(1/βsize, 1, βsize)
+ρsize = 100
+ρrng  = linspace(0.2/ρsize, 0.2, ρsize)
+ϵ 	  = 0.05
 
 mag   = zeros(βsize, ρsize)
 
 ## Evaluating the data points
 ################################
+tmax = 500
 for βi in βrng for ρi in ρrng
     soc  = Society(n=N, ρ=ρi, ϵ=ϵ)
     iter, x = metropolis!(soc, β=βi)
 
     summag = 0
-    for ti in 1:100
+    for ti in 1:tmax
     	summag += abs(magnetization(soc, x));
     	metropolisstep!(soc, x, βi)
     end
-    mag[findin(βrng, βi), findin(ρrng, ρi)] = summag / 100;
+    mag[findin(βrng, βi), findin(ρrng, ρi)] = summag / tmax;
 
     println("Evaluated point β=$βi, ρ=$ρi")
 end end
 
 ## Saving JLD file w/ results
 ################################
-save( "data/magnetization_sweep/magnetization_$(βsize)_$(ρsize)_ϵ$(ϵsize).jld", "mag", mag)
+save( "data/magnetization_sweep/magnetization_$(βsize)_$(ρsize)_ϵ$(ϵ).jld", "mag", mag)
 
 ## Some Plotting
 ###################
@@ -38,6 +39,6 @@ imshow(mag, extent=[0, 1, 0, 10], aspect="auto", origin="lower", interpolation="
 xlabel(L"\rho")
 ylabel(L"\beta")
 colorbar()
-title(L"Magnetization, $\epsilon =  0.1," * "\\ $(βsize) \\times $(ρsize)" * L"$ points")
+title(L"Magnetization, $\epsilon = $(ϵ)," * "\\ $(βsize) \\times $(ρsize)" * L"$ points")
 
-savefig( "data/magnetization_sweep/magnetization_$(βsize)_$(ρsize)_$(ϵsize)_rhobeta_eps01.png")
+savefig( "data/magnetization_sweep/magnetization_$(βsize)_$(ρsize)__$(ϵ)rhobeta.png")
